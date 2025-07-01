b8d299d7d52b02939454a0039a03d23b92277e7d
raka <rakakayla7@gmail.com>
2025-07-01 09:45:06 +0700

    add isi konten

--- first-react/src/body.jsx
+++ first-react/src/body.jsx
@@ -11,23 +11,33 @@ export default function MyMain(params) {
                     </p>
                 </div>
             </div>
-            <div className="h-20 p-3 bg-gray-100 mt-3 w-4/5 rounded-2xl flex justify-center items-start flex-col">
-                <p className=" font-semibold">Section</p>
-                <p className=" text-[8px] opacity-75">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis magni eveniet blanditiis sit voluptas ab sed eum, necessitatibus laudantium soluta?
+            <div className="h-20 bg-gray-100 mt-3 w-4/5 rounded-2xl flex justify-center items-start overflow-hidden">
+            <div className=" flex-1/3 bg-gray-200 h-full"></div>
+            <div className=" flex-1/1 flex flex-col justify-center p-3">
+                <p className=" font-semibold">IT Support</p>
+                <p className=" text-[8px] opacity-75">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
             </div>
-            <div className="h-auto mt-3 w-4/5 justify-between flex items-center rounded-xl flex-wrap bg-amber-50 overflow-hidden">
-                <div className="bg-gray-100 h-25 w-1/2 flex justify-center items-center">cnt</div>
-                <div className="bg-gray-200 h-25 w-1/2 flex justify-center items-center">cnt</div>
-                <div className="bg-gray-200 h-25 w-1/2 flex justify-center items-center">cnt</div>
-                <div className="bg-gray-100 h-25 w-1/2 flex justify-center items-center">cnt</div>
+            </div>
+            <div className="h-auto mt-3 w-4/5 justify-between flex items-center rounded-xl flex-wrap bg-amber-50 overflow-hidden z-10">
+                <div className="bg-gray-100 h-25 w-1/2 flex justify-center items-center">Linux</div>
+                <div className="bg-gray-200 h-25 w-1/2 flex justify-center items-center">Network</div>
+                <div className="bg-gray-200 h-25 w-1/2 flex justify-center items-center">Computer</div>
+                <div className="bg-gray-100 h-25 w-1/2 flex justify-center items-center">Ops</div>
             </div>
             <div className="h-20 bg-gray-100 mt-3 w-4/5 text-start flex-col p-3 rounded-2xl flex justify-center items-start">
-                <p className=" font-semibold">Section</p>
+                <p className=" font-semibold">Pendidikan</p>
                 <p className=" text-[8px] opacity-75">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis magni eveniet blanditiis sit voluptas ab sed eum, necessitatibus laudantium soluta?</p>
             </div>
-            <div className="h-50 bg-gray-100 w-4/5 mt-3 justify-center flex items-center rounded-3xl mb-3">myFoot</div>
-            <div className="h-20 bg-gray-100 w-4/5 rounded-2xl flex justify-center items-center">content</div>
+            <div className="h-50 bg-gray-100 w-4/5 mt-3 justify-center flex items-center rounded-3xl mb-3">Project</div>
+            <div className="h-20 bg-gray-100 w-4/5 rounded-2xl flex justify-center items-center overflow-hidden">
+                <div className=" flex-1/3 bg-gray-200 h-full"></div>
+                <div className=" flex-1/1 flex flex-col justify-center p-3">
+                    <p className=" font-semibold">BNSP</p>
+                    <p className=" text-[8px] opacity-75">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
